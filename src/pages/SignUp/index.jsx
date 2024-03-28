a3deb1b42ddde332ae1b1dcfc6c56e49c2fa885f
Douglas Rufino <douglasrufinosilva@gmail.com>
2024-03-28 12:10:42 -0300

    new functions added

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,7 @@
-import { Link } from "react-router-dom";
+import { useState } from "react"
+import { Link, useNavigate } from "react-router-dom"
+
+import { api } from "../../services/api"
 
 import { Container, Form, Background } from "./styles";
 import {FiUser, FiMail, FiLock} from 'react-icons/fi'
@@ -8,6 +11,32 @@ import { Button } from '../../components/Button'
 import { Input } from '../../components/Input'
 
 export function SignUp() {
+
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const navigate = useNavigate()
+
+  function handleSignUp() {
+    if(!name || !email || !password) {
+      return alert("Preencha todos os campos!")
+    }
+
+    api.post("/users", { name, email, password })
+    .then(() => {
+      alert("Usuário cadastrado com sucesso!")
+      navigate("/")
+    })
+    .catch(error => {
+      if(error.response) {
+        alert(error.response.data.message)
+      } else {
+        alert("Não foi possível cadastrar.")
+      }
+    })
+  }
+
   return(
     <Container>
 
@@ -22,22 +51,28 @@ export function SignUp() {
         <Input
           placeholder="Nome"
           type="text"
-          icon={FiUser} 
+          icon={FiUser}
+          onChange={e => setName(e.target.value)}
           />
 
         <Input
           placeholder="E-mail"
           type="text"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
           />
 
         <Input
           placeholder="Senha"
-          type="text"
+          type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
           />
 
-        <Button title="Cadastrar" />
+        <Button
+          title="Cadastrar"
+          onClick={handleSignUp}  
+        />
 
         <Link to="/">
           Voltar para login
